550335be6fff6ab4df8c89ad3c8e20677850a2bc
RoshanDasan <edroshan2001@gmail.com>
2023-10-04 20:49:58 +0530

    postgreSQl connected

--- admin/src/app.js
+++ admin/src/app.js
@@ -1,1 +1,22 @@
-console.log('haii');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var cors = require("cors");
+var typeorm_1 = require("typeorm");
+var AppDataSource = new typeorm_1.DataSource({
+    "type": "postgres",
+    "host": "localhost",
+    "port": 5432,
+    "username": "postgres",
+    "password": "r1o2s3h4a5n6",
+    "database": "microservice_demo",
+});
+AppDataSource.initialize().then(function () {
+    var app = express();
+    // cors setup for UI
+    app.use(cors({
+        origin: ["http://localhost:3000"]
+    }));
+    app.use(express.json());
+    app.listen(5000, function () { return console.log('Server connected'); });
+});
